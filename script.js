11abaaf57456faeae0bf8ae0803333ea7644e195
MD AQUIB EZAZ <115235361+20btcs037hy@users.noreply.github.com>
2024-11-24 12:19:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function insert_Row() {
     //Write your code here
-  
+  let item=document.getElementById("sampleTable");
+	const newRow = item.insertRow(0);
+
+  const cell1 = newRow.insertCell(0);
+  const cell2 = newRow.insertCell(1);
+
+  cell1.textContent = "New Cell 1";
+  cell2.textContent = "New Cell 2";
   
 }
